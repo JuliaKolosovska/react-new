41492bef71c206d69dba84847fddf0e5864031c4
JuliaKolosovska <Ykolosovska@gmail.com>
2022-09-14 11:17:47 +0200

    add lesson1.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {simpsons} from "./data";
 
 function App() {
+  let title='Simpsons'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>{title}</h2>
+        {
+            simpsons.map(simpson=> (<div>
+                    <h3>{simpson.name} {simpson.surname}</h3>
+                <p>Age: {simpson.age}</p>
+                <p>{simpson.info}</p>
+                <img src={simpson.photo}/>
+            </div>)
+            )
+        }
     </div>
   );
 }
