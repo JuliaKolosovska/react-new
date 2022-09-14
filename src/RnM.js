542998e7118b6279c0cd0c806f39282701ddea30
JuliaKolosovska <Ykolosovska@gmail.com>
2022-09-14 16:21:53 +0200

    add lesson1.2

--- src/RnM.js
+++ src/RnM.js
@@ -1,17 +1,17 @@
 import {useState} from "react";
 
 export default function RnM() {
-let [rnm, setRnm] = useState([]);
+    let [rnm, setRnm] = useState([]);
 
     fetch('https://rickandmortyapi.com/api/character')
         .then(value => value.json())
         .then(value => {
             setRnm(value);
-                    });
+        });
 
     return (
         <div>
-        {rnm.map((rn, id)=>(<RnM item={rn} key={id}/>))}
-    </div>
-    )
+            {rnm.map((rn, id) => (<RnM item={rn} key={id}/>))}
+        </div>
+    );
 }
