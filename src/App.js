cec6a2a3479e785c277a3534dd6b435b8c110387
JuliaKolosovska <Ykolosovska@gmail.com>
2022-09-14 14:39:17 +0200

    add lesson1.2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 import {simpsons} from "./data";
+import Simpsons from "./Simpsons";
 
 function App() {
-  let title='Simpsons'
-  return (
-    <div className="App">
-     <h2>{title}</h2>
-        {
-            simpsons.map(simpson=> (<div>
-                    <h3>{simpson.name} {simpson.surname}</h3>
-                <p>Age: {simpson.age}</p>
-                <p>{simpson.info}</p>
-                <img src={simpson.photo}/>
-            </div>)
-            )
-        }
-    </div>
-  );
+    let title='Simpsons'
+    return (
+        <div className="App">
+            <h2>{title}</h2>
+            {
+                simpsons.map(simpson=> (<Simpsons item={simpson}/>)
+                )
+            }
+        </div>
+    );
 }
 
 export default App;
