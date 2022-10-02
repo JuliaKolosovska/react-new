7b4c668081ef8ff00dce0c8fbd4dfe9af302f08f
JuliaKolosovska <Ykolosovska@gmail.com>
2022-10-02 21:14:23 +0200

    add lesson6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {useReducer} from "react";
+import {useForm} from "react-hook-form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const {register, handleSubmit} = useForm();
+const init = (initialValue) => {
+    return {cats: initialValue, dogs: initialValue}
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'AddCat':
+            return {...state, cats: state.cats}
+        case  'AddDog':
+            return {...state, dogs: state.dogs}
+    }
+    return {...state}
+}
+const onsubmit = (data) => {
+    console.log(data)
+};
+
+
+const  App =()=> {
+    const [state, dispatch] = useReducer(reducer,[], init);
+
+    return (
+        <div className="App">
+            <form onSubmit={() => handleSubmit(onsubmit)}>
+                <input {...register('Cat name')} type="text" placeholder={'Cat name'}/>
+                <button onClick={() => dispatch()}>Save</button>
+                <input type="text" placeholder={'Dog name'}/>
+                <button>Save</button>
+                <hr/>
+
+            </form>
+
+        </div>
+    );
 }
 
 export default App;
