edcffee2e75bc36a263298f87b13c7b6e8cccba9
JuliaKolosovska <Ykolosovska@gmail.com>
2022-10-09 18:31:55 +0200

    redux base

--- src/components/headers/Headers.js
+++ src/components/headers/Headers.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, {useEffect} from 'react';
+import {Link, useParams} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+
 import css from'./headers.module.css'
+import {LOAD_SINGLE_USER} from "../../redux";
 
 const Headers = () => {
+    const {user} = useSelector(state => state.usersReducer);
+
+    const dispatch = useDispatch();
+
+    const {id} = useParams();
+
+    useEffect(() => {
+        dispatch({type:LOAD_SINGLE_USER, payload: +id})
+    },[id]);
+
     return (
         <div className={css.nav_menu}>
             <div><Link to={'/'}>Main</Link></div>
             <div><Link to={'users'}>Users</Link></div>
             <div><Link to={'posts'}>Posts</Link></div>
             <div><Link to={'comments'}>Comments</Link></div>
+            <div>{user && (<div>{user.name}</div>)}</div>
         </div>
-    );
-};
-
+    )
+}
 export {Headers};
